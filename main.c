0459c6eac8cd136c02e061db9f50c8be1ac735a7
Filipe Cruz <bad-alloc-error@yandex.com>
2022-08-26 16:12:03 -0300

    teste main

---
+++ main.c
@@ -0,0 +1,35 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<stddef.h>
+#include"include/memleakdet.h"
+
+
+typedef struct estudante{
+    char nome[30];
+    char email[50];
+    unsigned int idade;
+}estudante_t;
+
+int main(){
+
+    struct_db_t* struct_db = create_struct_db();
+    estudante_t estudante1;
+
+    // strncpy(estudante1.nome, "Filipe", strlen("Filipe"));
+    // strncpy(estudante1.email, "bad-alloc-error@yandex.com", strlen("bad-alloc-error@yandex.com"));
+    // estudante1.idade = 28;
+
+    static field_info_t students_info[] = {
+        {"nome", sizeof(char) * 30, offsetof(estudante_t, nome), CHAR, 0},
+        {"email", sizeof(char) * 50, offsetof(estudante_t, email), CHAR, 0},
+        {"idade", sizeof(unsigned int), offsetof(estudante_t, idade), UINT32, 0}
+    };
+
+    db_rec_t* new_struct = register_structure(struct_db, "estudante1", sizeof(estudante_t), &students_info[0], 3);
+
+    print_struct_db(struct_db);
+    print_struct_info(new_struct);
+
+    return 0;
+}
